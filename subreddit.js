09267ecf38cd8a009b8f833dffc04bd345fca815
Emily Foley <foleyemilym@gmail.com>
2014-06-07 11:00:00 -0700

    fixing counter function

--- subreddit.js
+++ subreddit.js
@@ -6,12 +6,21 @@ $('#view').click(function(){
               if ( i < 20 ) return;
               $("#posts").append( '<div class="indv-post">' + '<ul>' + '<li>' + item.data.title + '</li>'
                 + item.data.url  + '</li>' + '<li>' + item.data.permalink + '</li>' + '</ul>' + 
-                '<div class="vote" id="up">' + '<a href="#">' +'</a>' + '</div>' + '<div class="vote" id="down"><a href="#"></a></div>' +
+                '<button class="vote" id="up" type="button">' + 'Vote up!' + '</button>' + '<div class="counter" id="countup">' + '</div>' + '<button class="vote" id="down" type="button">' + 'Vote down!'  + '</button>' +
+                '<div class="counter" id="countdown">' + '</div>' +
                 '</div>');
+
+                  $('#up').click(function(){
+                    $('#countup').html(function(i, val) { return val*1+1;
+                    });
+                  });
             });
           });
         });
       });
 });
 
+// $('#up').click(function(){
+//   $('#countup').html(function(i, val) { return val*1+1 });
+// });
 
