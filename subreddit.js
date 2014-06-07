ae4d28927fa9be962ff70e4ae94c67825ac4b889
Emily Foley <foleyemilym@gmail.com>
2014-06-07 08:55:43 -0700

    limited to 20 posts

--- subreddit.js
+++ subreddit.js
@@ -3,8 +3,9 @@ $('#view').click(function(){
     $('#latest').click(function(){
           $.getJSON("http://www.reddit.com/r/technology/new/.json?jsonp=?",function(data){
             $.each(data.data.children, function(i, item){
-              $("#posts").append( '<li>' + item.data.title + '</li>'
-                + item.data.url + '</li>' + '<li>' + item.data.permalink + '</li>' + '<br>');
+              if ( i < 20 ) return;
+              $("#posts").append( '<div class="indv-post">' + '<ul>' + '<li>' + item.data.title + '</li>'
+                + item.data.url + '</li>' + '<li>' + item.data.permalink + '</li>' + '</ul>' + '</div>');
             });
           });
         });
