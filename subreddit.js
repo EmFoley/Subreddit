24afd6999910b688dda267dc3b1c7beaaa5e0749
Emily Foley <foleyemilym@gmail.com>
2014-06-07 17:05:39 -0700

    dynamically incrementing post ids

--- subreddit.js
+++ subreddit.js
@@ -1,4 +1,4 @@
-$(function () {
+// $(function () {
   $('#view').click(function(){
     $('#posts').slideToggle("slow");
   });
@@ -6,23 +6,33 @@ $(function () {
 
   $('#latest').click(function(){
         $.getJSON("http://www.reddit.com/r/technology/new/.json?jsonp=?",function(data){
+          var counter = 0;
           $.each(data.data.children, function(i, item){
             if ( i < 20 ) return;
-            $("#posts").append( '<div class="indv-post">' + '<ul>' + '<li>' + item.data.title + '</li>'
+            $("#posts").append( '<div class="indv-post" id="number'+ counter+'">' + '<ul>' + '<li>' + item.data.title + '</li>'
               + item.data.url  + '</li>' + '<li>' + item.data.permalink + '</li>' + '</ul>' + 
-              '<div class="vote-up-div">' + '<button class="voteup" type="button">' + 'Vote up!' + '</button>' + '<div class="counter" id="countup">' + '</div>' + '</div>' + '<div class="vote-down-div"> '+ '<button class="vote" id="down" type="button">' + 'Vote down!'  + '</button>' +
+              '<div class="vote-up-div">' + '<button id="voteup" type="button">' + 'Vote up!' + '</button>' + '<div class="counter" id="countup">' + '</div>' + '</div>' + '<div class="vote-down-div"> '+ '<button id="votedown" type="button">' + 'Vote down!'  + '</button>' +
               '<div class="counter" id="countdown">' + '</div>' + '</div>' +
               '</div>');
+            counter++;
           });
         });
       });
-      
-   $(".vote-up-div").on("click",function() {
-       $('this').find('.counter').html(function(i, val) { return +val+1 });
-   });
-   $(".vote-down-div").on("click",function() {
-       $('this').find('.counter').html(function(i, val) { return +val+1 });
-   });
 
- });
+        // $('#voteup').on("click", function() {
+        //   $('#countup').html(function(i, val) { return +val+1 });
+        //   });
+        //  $("#down").click(function() {
+        //   $('#countdown').html(function(i, val) { return +val+1 });
+        //    });
+      
+   // $(".vote-up-div").click(function() {
+   //  console.log("anything");
+   
+   // });
+   // $(".votedown").on("click",function() {
+   //     $('this').find('.counter').html("function(i, val) { return +val+1 }");
+   // });
+
+ // });
 
