eae6b1ed31b6d1dada8bb8761f507b7135494152
Emily Foley <foleyemilym@gmail.com>
2014-06-08 20:19:24 -0700

    back to it

--- subreddit.js
+++ subreddit.js
@@ -2,28 +2,40 @@ $( document ).ready(function() {
   $('#view').click(function(){
     $('#posts').slideToggle("slow");
   });
+});
 
 
-  $('#latest').click(function(){
-        $.getJSON("http://www.reddit.com/r/technology/new/.json?jsonp=?",function(data){
-          var counter = 0;
-          $.each(data.data.children, function(i, item){
-            if ( i < 20 ) return;
-            $("#posts").append( '<div class="indv-post" id="number'+ counter+'">' + '<ul>' + '<li>' + item.data.title + '</li>'
-              + item.data.url  + '</li>' + '<li>' + item.data.permalink + '</li>' + '</ul>' + 
-              '<div class="vote-up-div">' + '<button id="voteup" type="button">' + 'Vote up!' + '</button>' + '<div class="counter" id="voteup">' + '</div>' + '</div>' + '<div class="vote-down-div"> '+ '<button id="votedown" type="button">' + 'Vote down!'  + '</button>' +
-              '<div class="counter" id="countdown">' + '</div>' + '</div>' +
-              '</div>');
-            counter++;
-          });
+$('#latest').click(function(){
+      $.getJSON("http://www.reddit.com/r/technology/new/.json?jsonp=?",function(data){
+        var counter = 0;
+        $.each(data.data.children, function(i, item){
+          if ( i < 20 ) return;
+          $("#posts").append( '<div class="indv-post" id="number'+ counter+'">' + '<ul>' + '<li>' + item.data.title + '</li>'
+            + item.data.url  + '</li>' + '<li>' + item.data.permalink + '</li>' + '</ul>' + 
+            '<div class="vote-up-div">' + '<button id="voteup" type="button">' + 'Vote up!' + '</button>' + '<div class="counter" id="voteup">' + '<p></p>' + '</div>' + '</div>' + '<div class="vote-down-div"> '+ '<button id="votedown" type="button">' + 'Vote down!'  + '</button>' +
+            '<div class="counter" id="countdown">' + '</div>' + '</div>' +
+            '</div>');
+          counter++;
         });
       });
+    });
 
-  $('#voteup').on("click", (function(){
-    $(this).parent().children('.counter').html(function(i, val) { return +val+1 });
-  }));
-  
-  $('#voteup').on("click", (function(){
-    $(this).parent().children('.counter').html(function(i, val) { return +val+1 });
-  }));
+
+$('.vote-up-div').on("click", "p", function(){
+  var clickNum = 0;
+  $(this).html(clickNum++);
 });
+
+//   $('#voteup').on('click', (function(){
+//     var clickNum = 0;
+//     $(this).parent().children('.counter').html(clickNum ++);
+//   }));
+  
+//   $('#voteup').on('click', (function(){
+//     $(this).parent().children('.counter').html( clickNum ++ );
+//   }));
+// });
+
+// var reOrder = function() {
+
+// }
